64fa93f6f27d20f207f13f9451a2caeab6de382b
dahill0001 <4821143@qq.com>
2022-11-23 16:42:56 +0800

    Init

---
+++ langcheck/src/main/java/com/meicai/langcheck/LangcheckApplication.java
@@ -0,0 +1,14 @@
+package com.meicai.langcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class LangcheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LangcheckApplication.class, args);
+	}
+
+}
